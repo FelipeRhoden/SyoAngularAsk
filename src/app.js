93e5a4d1ee94d5480bf86bf9b3236b6586b1942c
Felipe Rhoden <felipe.rhoden@syonet.com>
2024-09-12 21:44:28 -0300

    feat(): create Sala AngularJS

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
 import "./style.css";
-import "angular";
+import angular from "angular";
+
+const room = angular.module("room", [])
+
+room.controller("questionController", ['$scope', function($scope) {
+
+    $scope.author = "Rhoden";
+
+    $scope.questions = [
+        { likeCount: 0, content: "Quando o AngularJS foi criado?", author: "Rhoden" },
+        { likeCount: 1, content: "O que é um controller?", author: "Pohren" },
+        { likeCount: 1, content: "Alguém ainda usa AngularJS?", author: "Silva" }
+    ]
+
+    $scope.ask = () => {
+        if ( !$scope.newQuestion ) {
+            return;
+        }
+        $scope.questions.unshift(
+            { likeCount: 0, content: $scope.newQuestion, author: $scope.author },
+        );
+        $scope.newQuestion = "";
+    }
+}])
